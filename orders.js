9b4c86150476dbb22b2d1d8ba6edb99b194e0f76
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-14 22:33:22 +0300

    feat: add DOM selector for orders injection target.

--- orders.js
+++ orders.js
@@ -1,3 +1,6 @@
 //GET ORDERS FROM LOCALSTORAGE.
 //Get the orders array from localStorage, if none exist use empty array.
 let orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+//Select the ordersGrid div where cards will be added. 
+const ordersGrid = document.getElementById("ordersGrid");
