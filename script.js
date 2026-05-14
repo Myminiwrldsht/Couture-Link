dccd4a506694637aa2ea6d9e46503aee15dc71db
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-14 19:41:07 +0300

    feat: add form submit event listener and prevent default page refresh.

---
+++ script.js
@@ -0,0 +1,9 @@
+//Select the form using it's ID, listens for when the user submits it, and prevents the page from refreshing by default.
+//Allows us to handle the form submission with JavaScript instead of the default behavior.
+document.getElementById("submissionForm").addEventListener("submit", function(event) {
+    
+    //Prevents the browser's default form submission, lets JS habdle it.
+    event.preventDefault()
+    
+}); 
+
